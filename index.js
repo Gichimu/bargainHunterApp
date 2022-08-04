19b879b7bd930b95f260e4deac407a2baee4e020
Gichimu <gichimueric12@gmail.com>
2022-08-04 11:11:30 +0300

    fix the home route error

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const express = require('express');
+const express = require("express");
 
 const path = require("path");
 
-const cors = require('cors');
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
-const route = require('./scraper');
+const route = require("./scraper");
 
-const app = express()
+const app = express();
 
 app.use(express.static(path.join(__dirname, "./public")));
 
 // cors
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
 // middleware
 app.use(express.json());
 
-// add route middleware
-app.use('/api', route);
+app.get("/home", function (req, res) {
+  res.redirect("/");
+});
 
-app.listen(PORT, () => { console.log(`Server running on port ${PORT}`)})
+// add route middleware
+app.use("/api", route);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
